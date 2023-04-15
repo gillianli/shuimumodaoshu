898c34bd8a2ea6c41ab2dd0a20e22c3cab089aa1
gillianli <georgeli2012@gmail.com>
2023-04-15 01:42:46 -0400

    firt commit

---
+++ utils.js
@@ -0,0 +1,86 @@
+/**
+ * 创建一个 html element
+ * @param name
+ * @param params
+ * @returns {*}
+ */
+function createElement(name, params) {
+    let element = document.createElement(name);
+    for (let key in params) {
+        element.setAttribute(key, params[key]);
+    }
+    return element;
+}
+
+function toast(text, time) {
+    let toast = document.getElementById('toast');
+    let toast_box = document.getElementsByClassName('toast_box')[0];
+    toast.innerHTML = text;
+    toast_box.style.animation = 'show 1.5s';
+    toast_box.style.display = 'inline-block';
+    setTimeout(function () {
+        toast_box.style.animation = 'hide 1.5s';
+        setTimeout(function () {
+            toast_box.style.display = 'none';
+        }, 1400)
+    }, time)
+}
+
+function copyToClip(content, t2) {
+    const inputDom = document.createElement('input');
+    inputDom.setAttribute('value', content);
+    document.body.appendChild(inputDom);
+    inputDom.select();
+    document.execCommand('copy');
+    document.body.removeChild(inputDom);
+    if (t2 !== false) {
+        toast("已复制到剪贴板")
+    }
+
+}
+
+function injectPosToWebUi(injectInfo) {
+    try {
+        if (chrome == null || chrome.tabs == null) {
+            return;
+        }
+        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+            chrome.scripting.executeScript({
+                target: { tabId: tabs[0].id },
+                func: () => {
+                    window.novelAi_inject_pos = injectInfo;
+                }
+            });
+            chrome.scripting.executeScript({
+                target: { tabId: tabs[0].id },
+                files: ['injectPos.js']
+            });
+        });
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+function injectNegToWebUi(injectInfo) {
+    try {
+        if (chrome == null || chrome.tabs == null) {
+            return;
+        }
+        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+            chrome.scripting.executeScript({
+                target: { tabId: tabs[0].id },
+                func: () => {
+                    window.novelAi_inject_neg = injectInfo;
+                }
+            });
+            chrome.scripting.executeScript({
+                target: { tabId: tabs[0].id },
+                files: ['injectNeg.js']
+            });
+        });
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+
